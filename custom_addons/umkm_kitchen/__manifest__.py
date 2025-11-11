8b97bce6aa5ad7d3f50d8a21ccf553d2f249755a
iamyourdre <iamyourdre@gmail.com>
2025-11-12 06:35:20 +0700

    Add initial configuration files for UMKM Kitchen Management module

---
+++ custom_addons/umkm_kitchen/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'UMKM Kitchen Management',
+    'version': '17.0.1.0.0',
+    'category': 'Sales/Point of Sale',
+    'summary': 'Odoo module for managing kitchen orders in UMKM settings',
+    'author': 'Adrian Sutansaty',
+    'website': 'https://test.com',
+    'depends': ['base', 'mail'],
+    'data': [
+      
+    ],
+    'installable': True,
+    'application': True,
+}
